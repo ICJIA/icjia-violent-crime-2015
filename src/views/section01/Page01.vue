c99379951e73baf2872bf02d700851593e2c03e3
cschweda <cschweda@gmail.com>
2016-12-08 13:54:02 -0600

    removed deferred rendering

--- src/views/section01/Page01.vue
+++ src/views/section01/Page01.vue
@@ -124,26 +124,26 @@
 
             Highcharts.chart('container1', hc1);
 
-            (function (H) {
-                function deferRender (proceed) {
-            var series = this, 
-            $renderTo = $(this.chart.container.parentNode);
+        //     (function (H) {
+        //         function deferRender (proceed) {
+        //     var series = this, 
+        //     $renderTo = $(this.chart.container.parentNode);
 
-            // It is appeared, render it
-            if ($renderTo.is(':appeared') || !series.options.animation) {
-            proceed.call(series);
+        //     // It is appeared, render it
+        //     if ($renderTo.is(':appeared') || !series.options.animation) {
+        //     proceed.call(series);
             
-            // It is not appeared, halt renering until appear
-            } else  {
-                $renderTo.appear(); // Initialize appear plugin
-                $renderTo.on('appear', function () {
-                proceed.call(series);
-                });
-                }
-            };
-            H.wrap(H.Series.prototype, 'render', deferRender);
+        //     // It is not appeared, halt renering until appear
+        //     } else  {
+        //         $renderTo.appear(); // Initialize appear plugin
+        //         $renderTo.on('appear', function () {
+        //         proceed.call(series);
+        //         });
+        //         }
+        //     };
+        //     H.wrap(H.Series.prototype, 'render', deferRender);
     
-         }(Highcharts));
+        //  }(Highcharts));
             //Highcharts.chart('container2', hc2);
             $('.carousel.carousel-slider').carousel({full_width: true});
             // var numeral = require('numeral');
