c855606f163fa6467b6cee307149f09684ae2d99
cschweda <cschweda@gmail.com>
2016-12-14 14:25:48 -0600

    fixed typo

--- src/components/RenderChart.vue
+++ src/components/RenderChart.vue
@@ -23,7 +23,7 @@
     // Datatables install with webpack: https://gist.github.com/marcstober/c34bb4bdf7ef622cb24d6675723749bd
     // https://datatables.net/forums/discussion/32542/datatables-and-webpack
     import dt from 'datatables.net';
-    import 'datatables.net-dt/css/jquery.datatables.css';
+    import 'datatables.net-dt/css/jquery.dataTables.css';
     
     // Import all the chart/table elements
     import * as section01Elements from '../charts/section01.js'
