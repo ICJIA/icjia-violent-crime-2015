9d57a1618bd19874c2b660ded2a5fbc4d89f19cb
cschweda <cschweda@gmail.com>
2016-12-14 16:04:12 -0600

    added sample slider to sec1 - page2

--- src/views/section01/Page02.vue
+++ src/views/section01/Page02.vue
@@ -24,10 +24,28 @@
 
     <div v-phtext:1p7s></div>
 
-  <render-chart
-        chart="hc005_chart"  
-    >
-    </render-chart>
+  <div class="carousel carousel-slider center" data-indicators="true">
+    <div class="carousel-fixed-item center">
+      <a class="btn waves-effect white grey-text darken-text-2">button</a>
+    </div>
+    <div class="carousel-item red white-text" href="#one!">
+      <h2>First Panel</h2>
+      <p class="white-text">This is the first panel</p>
+    </div>
+    <div class="carousel-item amber white-text" href="#two!">
+      <h2>Second Panel</h2>
+      <p class="white-text">This is the second panel</p>
+    </div>
+    <div class="carousel-item green white-text" href="#three!">
+      <h2>Third Panel</h2>
+      <p class="white-text">This is the third panel</p>
+    </div>
+    <div class="carousel-item blue white-text" href="#four!">
+      <h2>Fourth Panel</h2>
+      <p class="white-text">This is the fourth panel</p>
+    </div>
+  </div>
+        
 
     <div v-phtext:2p3s></div>
 
@@ -50,6 +68,7 @@ export default {
     methods: {
         
     },
+    
     components: {
         
         RenderChart
