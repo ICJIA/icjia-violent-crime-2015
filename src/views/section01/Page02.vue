231aaac3d123716ddac1a0c91e86bb12dfa1a548
cschweda <cschweda@gmail.com>
2016-12-14 16:14:36 -0600

    removed carousel

--- src/views/section01/Page02.vue
+++ src/views/section01/Page02.vue
@@ -24,27 +24,7 @@
 
     <div v-phtext:1p7s></div>
 
-  <div class="carousel carousel-slider center" data-indicators="true">
-    <div class="carousel-fixed-item center">
-      <a class="btn waves-effect white grey-text darken-text-2">button</a>
-    </div>
-    <div class="carousel-item red white-text" href="#one!">
-      <h2>First Panel</h2>
-      <p class="white-text">This is the first panel</p>
-    </div>
-    <div class="carousel-item amber white-text" href="#two!">
-      <h2>Second Panel</h2>
-      <p class="white-text">This is the second panel</p>
-    </div>
-    <div class="carousel-item green white-text" href="#three!">
-      <h2>Third Panel</h2>
-      <p class="white-text">This is the third panel</p>
-    </div>
-    <div class="carousel-item blue white-text" href="#four!">
-      <h2>Fourth Panel</h2>
-      <p class="white-text">This is the fourth panel</p>
-    </div>
-  </div>
+  
         
 
     <div v-phtext:2p3s></div>
